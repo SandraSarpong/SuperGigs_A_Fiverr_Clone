a45eab11a5f531df03f3d0ce0e38ed0afb0b4b57
Ahercode <philipkelly407@gmail.com>
2023-03-28 21:46:20 +0000

    fix vite error

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -43,7 +43,7 @@ function Gigs() {
   return (
     <div className="gigs">
       <div className="container">
-        <span className="breadcrumbs">Liverr > Graphics & Design ></span>
+        <span className="breadcrumbs">Liverr - Graphics & Design -</span>
         <h1>AI Artists</h1>
         <p>
           Explore the boundaries of art and technology with Liverr's AI artists
--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://cors-anywhere.herokuapp.com/https://clever-clam-visor.cyclic.app/api/",
   withCredentials: true,
 });
 
